200f375f18add6a0258fe091131d5740ddda8727
Joshua Vial <joshua@enspiral.com>
2015-08-31 11:49:27 +1200

    adding convience methods to rails spec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,10 +25,13 @@ require 'rspec/rails'
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+ActiveRecord::Migration.check_pending!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.infer_spec_type_from_file_location!
+  config.include FactoryGirl::Syntax::Methods
+  #config.include Devise::TestHelpers, type: :controller
+  config.include Rails.application.routes.url_helpers
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -48,5 +51,5 @@ RSpec.configure do |config|
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
-  config.infer_spec_type_from_file_location!
+  #config.infer_spec_type_from_file_location!
 end
