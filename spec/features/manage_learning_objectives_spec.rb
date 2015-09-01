a19277e8c7025886ea0ee0d83562aa6b25392050
Jory & Michael Wilson <pair+joryaku.michaelwilsonM@devbootcamp.com>
2015-09-02 10:24:41 +1200

    Refactor manage_learning_objectives_spec
    add before block and changed learning objectives
    path

--- spec/features/manage_learning_objectives_spec.rb
+++ spec/features/manage_learning_objectives_spec.rb
@@ -1,29 +1,26 @@
 require 'rails_helper'
 
 RSpec.feature "Learning objectives" do
+
+  before do
+    visit learning_objectives_path
+  end
+
   it "shows learning objectives page" do
-    visit('/learning_objectives')
     expect(page.body).to have_css(".objectives")
   end
 
-  #we need a form with 2 input fields
-
   it "shows a form with two inputs" do
-    visit('/learning_objectives')
     fill_in("Title", with:  "Jory's cheat sheet")
     fill_in("Description", with:  "capybara has a cool cheat sheet")
     click_button('submit')
 
     expect(page).to have_content("Jory's cheat sheet")
     expect(page).to have_content("capybara has a cool cheat sheet")
-
   end
 
   it "clicks submit button and takes to new page" do
-    visit('/learning_objectives')
     click_button('submit')
     expect(page.body).to have_content('Success')
   end
-
-#I want to see form info on the view_objectives page
 end
