6f33e72f4e429755d2fdcd2774fcafc615c1f8d3
Piet Geursen <pietgeursen@gmail.com>
2015-09-03 15:06:08 +1200

    Pass test with stubbed out user

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,7 @@
 class ProfileController < ApplicationController
 
  def show
-   @token = session[:github_token]
+	 @token = session[:github_token]
+	 @token ? @token : (redirect_to root_path)
  end
 end
--- spec/features/stubbed_users_spec.rb
+++ spec/features/stubbed_users_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "StubbedUsers", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+	context 'User is not logged in' do
+		it "visiting profile page redirects home" do
+			visit profile_path
+			expect(page).to have_content('KaiAko')
+		end
+	end
+
+	context 'User is logged in by stubbing out oauth' do
+		it "visiting profile page displays profile" do
+			visit root_path
+			click_on 'Log In with Github'
+			expect(page).to have_content('Log Out')
+		end
+	end
+
 end
