2454ed2ec25454f797d854f411af5cde1a700b90
Anna Lezhikova & Sarah Rogers <pair+annavlz.sarah-arrrgh@devbootcamp.com>
2015-09-03 15:12:47 +1200

    Fix feature test for emo_state

--- spec/features/emo_state_spec.rb
+++ spec/features/emo_state_spec.rb
@@ -7,13 +7,13 @@ require 'rails_helper'
     end
 
     it 'user can see emotional state form' do
-      expect(page).to have_css('form#emo-state')
+      expect(page).to have_css('div#emo-state')
     end
 
     it 'user can submit emotional state form' do
       within('#emo-state') do
-        choose('emo_state_happygit ')
-        click_button('Submit')
+        choose('emo_state_emotion_happy')
+        click_button('Save Emo state')
       end
       expect(current_path).to eq('/')
     end
