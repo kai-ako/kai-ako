1bc1978277e0acaeaa259582d660a913a4fc718c
Anna Lezhikova & Michael Wilson <pair+annavlz.michaelwilsonm@devbootcamp.com>
2015-09-07 10:43:50 +1200

    Add emo_states test for new page

--- spec/features/emo_states_list_spec.rb
+++ spec/features/emo_states_list_spec.rb
@@ -10,6 +10,11 @@ require 'rails_helper'
       expect(page).to have_css("a#emo_state_links", :text => 'See all')
     end
 
+    it 'user can visit the emostate list page' do
+      click_link 'See all'
+      expect(current_url).to eq('index_emo_state_path')
+    end
+
     # it 'user can submit emotional state form' do
     #   within('#emo-state') do
     #     choose('emo_state_emotion_happy')
