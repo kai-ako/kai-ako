80f0d3e4cf66e8774bbceeab887fe846aa3d2066
Apprentice <apprentice@StMac8.local>
2015-09-08 11:08:19 +1200

    Correct minor bug in 'Curriculum_spec.rb' test for non existent details_tree

--- spec/features/curriculums_spec.rb
+++ spec/features/curriculums_spec.rb
@@ -9,20 +9,19 @@ RSpec.feature "Curriculums", type: :feature do
    it "has a curriculum link on the home page" do
 	   visit "/"
 	   expect(page.body).to have_content("Curriculum")
-	   # expect("Curriculum").to have_css("a") 
+	   # expect("Curriculum").to have_css("a")
    end
-   
+
    before(:each) do
    	nav
    end
    it "has the heading 'Curriculum Tree'" do
-   	expect(page.body).to have_content("Curriculum Tree") 
+   	expect(page.body).to have_content("Curriculum Tree")
    end
 
    it "has a details element on the page" do
-   	expect(page.body).to have_css("#{}details_tree")
+   	expect(page.body).to have_css("#details_tree")
    end
-   it ""
 
 end
 
