f4268670e0b2db7740ea5b1ca6dfda74586a59d3
Jory <email": null>
2015-09-21 11:13:57 +1200

    Adjust learning_objective controller test

--- spec/controllers/learning_objectives_controller_spec.rb
+++ spec/controllers/learning_objectives_controller_spec.rb
@@ -1,17 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe LearningObjectivesController, type: :controller do
-  describe "#complete" do
-    let!(:learning_objective) {create(:learning_objective)}
-    let(:complete) { post :complete, id: learning_objective.id }
 
-    before { complete }
-    it 'updates the models completed_at field' do
-      expect(learning_objective.reload.completed_at).to_not be_nil
-    end
-    it 'redirects to the learning objectives index page' do
-      expect(response).to redirect_to learning_objectives_path
+  describe 'GET #index' do
+
+    it 'has a 200 status' do
+      get :index
+      expect(response.status).to eq(200)
     end
 
+    it 'index should render index template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
   end
+
+  xdescribe 'POST #complete' do
+
+    xit 'redirects to learning_objectives_path' do
+      post :complete
+
+      expect(response).to be(learning_objectives_path)
+    end
+  end
+
+   describe 'POST #create' do
+
+    it ''
+   end
+
+  # describe 'GET #new' do
+  # end
+
+  # describe 'GET #edit' do
+  # end
+
+  # describe 'GET #show' do
+  # end
+
+  # describe 'PATCH #update' do
+  # end
+
+  # describe 'PUT #update' do
+  # end
+
+  # describe 'DELETE #destroy' do
+  # end
 end
+
